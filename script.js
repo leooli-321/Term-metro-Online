9c8747add5da729fcef284c0b3209825a53f7c75
Leo Oli <111819459+leooli-321@users.noreply.github.com>
2022-09-08 01:16:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// (0 °C × 9/5) + 32 = 32 °F
+//var b = (9 / 5) + 32;
+
+
+
+
+function play() {
+
+
+    let numero = document.getElementById("numero").
+    value;
+
+
+    let far = (numero * 9/5) + 32;
+
+    alert("São " + far + " fahrenheit!");
+
+    }
+
+
+
